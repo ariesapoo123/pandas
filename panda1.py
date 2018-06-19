7f44f1940890165a8d5c6b01f1a60c07c44475f1
ariesapoo123 <35699069+ariesapoo123@users.noreply.github.com>
2018-06-19 21:35:10 +0530

    Update panda1.py

--- panda1.py
+++ panda1.py
@@ -6,3 +6,11 @@ data = {'Name': ['Apoo', 'Anie'], 'Age': [20,21], 'Email id' : ['apoorvajai0@gma
 df = pd.DataFrame(data)
 
 print(df)
+
+
+output
+
+
+  Name  Age                   Email id     PhoneNo
+0  Apoo   20      apoorvajai0@gmail.com  9045287058
+1  Anie   21  aniketkumar.ray@gmail.com  7017370671
